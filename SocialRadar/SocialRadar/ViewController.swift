7a472254c7594b5a49ed5a7c38df13fad1a19813
Thawee T <th.thawee@gmail.com>
2014-10-31 10:14:53 +0700

    New project with Single-Page app template

---
+++ SocialRadar/SocialRadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialRadar
+//
+//  Created by Thawee on 10/31/14.
+//  Copyright (c) 2014 dsthackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
