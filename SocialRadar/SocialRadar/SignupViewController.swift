a812f43ee94dce4cda238c92198993df79341145
sasitorn <sasitorn.oeam@gmail.com>
2014-10-31 19:10:11 +0700

    Added login and signup controllers

---
+++ SocialRadar/SocialRadar/SignupViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignupViewController.swift
+//  SocialRadar
+//
+//  Created by Sasitorn Aramcharoenrat on 10/31/2557 BE.
+//  Copyright (c) 2557 dsthackathon. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func performSignup(sender: UIButton) {
+    }
+    @IBAction func gotoLoginPage(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
