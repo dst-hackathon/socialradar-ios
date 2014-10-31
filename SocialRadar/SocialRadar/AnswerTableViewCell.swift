c17abb86832961ac65ff78e643147d2bcd64df34
oreoblizzard <prakrit.tan@gmail.com>
2014-10-31 19:45:01 +0700

    Add Question Controller

---
+++ SocialRadar/SocialRadar/AnswerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerTableViewCell.swift
+//  SocialRadar
+//
+//  Created by Prakrit Tanthawatkul on 10/31/14.
+//  Copyright (c) 2014 dsthackathon. All rights reserved.
+//
+
+import UIKit
+
+class AnswerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SocialRadar/SocialRadar/QuestionTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  QuestionTableViewController.swift
+//  SocialRadar
+//
+//  Created by Prakrit Tanthawatkul on 10/31/14.
+//  Copyright (c) 2014 dsthackathon. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewController: UITableViewController {
+
+    var items: [String] = ["We", "Heart", "Swift"]
+    let navigationTitle = "Questions"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.navigationItem.title = navigationTitle
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = self.items[indexPath.row]
+        
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+        
+        self.performSegueWithIdentifier("selectedQuestion", sender: self)
+    }
+}
